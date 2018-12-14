020999a4dd25ea7d8461c0c6a012e6c8f48e6251
nikoletakuneva <NI>
2018-12-14 14:57:12 +0000

    Final touches.

--- app/src/androidTest/java/com/example/nikoleta/coinz/SignUpAndDisplayUsernameTest.java
+++ app/src/androidTest/java/com/example/nikoleta/coinz/SignUpAndDisplayUsernameTest.java
@@ -1,17 +1,14 @@
 package com.example.nikoleta.coinz;
 
-
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.filters.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,10 +19,8 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -40,151 +35,75 @@ public class SignUpAndDisplayUsernameTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.signUpButton), withText("Register"),
-                        childAtPosition(
-                                allOf(withId(R.id.linearLayout),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                3),
-                        isDisplayed()));
-        appCompatButton.perform(click());
+        onView(withId(R.id.signUpButton)).perform(click());
 
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction editText = onView(
-                allOf(withId(R.id.email),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.emailLayout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        editText.perform(replaceText("test@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.email)).perform(replaceText("testingTests@gmail.com"), closeSoftKeyboard());
 
-        ViewInteraction editText2 = onView(
-                allOf(withId(R.id.email), withText("test@gmail.com"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.emailLayout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        editText2.perform(pressImeActionButton());
+        onView(withId(R.id.email)).perform(pressImeActionButton());
 
-        ViewInteraction editText3 = onView(
-                allOf(withId(R.id.password),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.passwordLayout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        editText3.perform(replaceText("123456"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(replaceText("123456"), closeSoftKeyboard());
 
-        ViewInteraction button = onView(
-                allOf(withId(R.id.signUpButton), withText("Sign Up"),
-                        childAtPosition(
-                                allOf(withId(R.id.linearLayout2),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                2),
-                        isDisplayed()));
-        button.perform(click());
+        onView(withId(R.id.signUpButton)).perform(click());
 
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.username),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.usernameLayout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatEditText.perform(click());
+        onView(withId(R.id.username)).perform(click());
 
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(5000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.username),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.usernameLayout),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatEditText2.perform(replaceText("Test"), closeSoftKeyboard());
+        onView(withId(R.id.username)).perform(replaceText("Testing Tests"), closeSoftKeyboard());
 
-        ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.usernameButton), withText("Create Username"),
-                        childAtPosition(
-                                allOf(withId(R.id.linearLayout3),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatButton2.perform(click());
+        onView(withId(R.id.usernameButton)).perform(click());
 
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(60000);
+            Thread.sleep(8000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        onView(withId(R.id.textView1)).check(matches(withText("Hello Test!")));
-
-
+        onView(withId(R.id.textView1)).check(matches(withText("Hello Testing Tests!")));
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+    @After
+    public void deleteAccount(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        firebaseAuth.signOut();
+        if (user != null) {
+            db.collection("users").document(user.getUid()).delete();
+            user.delete();
+        }
     }
 }
